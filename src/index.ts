b2c458fdd26c0a7c93e403fd3b014fa142a3cf32
Matheus Amarante <matheusamarante1@gmail.com.br>
2023-06-17 11:34:41 -0300

    precisso arrumar as senhas

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ import compression from 'compression';
 import cors from 'cors';
 import mongoose, { connect } from 'mongoose';
 
+import router from './router';
+
 
 const app = express();
 
@@ -31,3 +33,4 @@ mongoose.connect(MONGO_URL);
 mongoose.connection.on('error', (error: Error) => console.log(error));
 
 
+app.use('/', router());
